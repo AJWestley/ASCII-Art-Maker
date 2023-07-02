6cdaaf48a68da4abe6d9faa957e352fc56ec48a5
AJ <alexanderjwestley@gmail.com>
2023-07-02 12:08:49 +0200

    Refactored to an OOP model

--- ASCII.py
+++ ASCII.py
@@ -1,42 +1,50 @@
-import os
 import cv2
-import numpy as np
+import os
 
-def import_adjust(path: str, ideal_width: int = 100):
-    
-    # import image, then greyscale and resize
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    scale_factor = ideal_width / img.shape[1]
-    scaled_dimensions = (int(img.shape[1] * scale_factor), int(img.shape[0] * scale_factor * 0.6)) # extra height scale for rectangular cells
-    
-    return cv2.resize(img, scaled_dimensions, interpolation=cv2.INTER_AREA)
+class ASCII_Artwork:
+    def __init__(self) -> None:
+        self.image = []
+        self.ascii_image = []
 
-def pixel_to_ascii(pixel_val: int) -> str:
-    ascii_vals = ['%', '#', '+', '*', '=', '-', ':', '.', ' ']
-    
-    # formula to get an ascii index from a pixel value
-    # 0->25 = index 0 then index goes up every 30
-    pixel_val += 4
-    i = pixel_val // 30
-    
-    return ascii_vals[i]
+    def translate(self, inv: bool = False):
+        self.ascii_image = []
+        for row in self.image:
+            ascii_row = [ASCII_Artwork.pixel_to_ascii(pixel, inv) for pixel in row]
+            self.ascii_image.append(ascii_row)
 
-def translate(image):
-    ascii_image = []
-    for row in image:
-        ascii_row = [pixel_to_ascii(pixel) for pixel in row]
-        ascii_image.append(ascii_row)
-    return ascii_image
-
-def print_ascii(ascii_image):
-    print()
-    for row in ascii_image:
-        for pixel in row:
-            print(pixel, end='')
+    def print_ascii(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
         print()
+        for row in self.ascii_image:
+            for pixel in row:
+                print(pixel, end='')
+            print()
+            
+    def import_adjust(self, path: str, ideal_width: int = 100, height_scale_factor: float = 0.6):
+        
+        # import image, then greyscale and resize
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        scale_factor = ideal_width / img.shape[1]
+        scaled_dimensions = (int(img.shape[1] * scale_factor), int(img.shape[0] * scale_factor * height_scale_factor))
+        
+        self.image =  cv2.resize(img, scaled_dimensions, interpolation=cv2.INTER_AREA)
+
+    @staticmethod
+    def pixel_to_ascii(pixel_val: int, inv: bool = False) -> str:
+        ascii_vals = ['%', '#', '+', '*', '=', '-', ':', '.', ' ']
+        
+        # formula to get an ascii index from a pixel value
+        # 0->25 = index 0 then index goes up every 30
+        # if inv = true, the index is reversed
+        pixel_val += 4
+        i = 8 - pixel_val // 30 if inv else pixel_val // 30
+        
+        return ascii_vals[i]
 
 if __name__ == '__main__':
-    os.system('cls' if os.name == 'nt' else 'clear')
-    image = import_adjust('spaceguy.jpg', 150)
-    ascii_image = translate(image)
-    print_ascii(ascii_image)
+    path = input('Enter image path: ')
+    size = int(input('Enter desired image width: '))
+    image = ASCII_Artwork()
+    image.import_adjust(path, size)
+    image.translate()
+    image.print_ascii()
